1f6c5fdc6a91fa5d73b9753a165920ee9a08982b
DoofisS <thevlad2698@gmail.com>
2024-12-10 19:44:15 +0200

    add new test data

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import icon from 'astro-icon'
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  site: 'https://turn-digital.github.io/accessible-starter-astro/',
+  site: 'https://turn-digital.github.io',
   integrations: [
     mdx(),
     icon(),
