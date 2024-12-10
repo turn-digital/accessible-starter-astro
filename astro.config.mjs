df82b69102d6b10d09471b62d0c3e40ff94519c2
DoofisS <thevlad2698@gmail.com>
2024-12-10 19:27:48 +0200

    add site to config for css  and deploy file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,18 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 import compress from 'astro-compress'
-import icon from "astro-icon"
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  integrations: [mdx(), icon(), tailwind({
-    applyBaseStyles: false,
-  }), compress()],
+  site: 'https://turn-digital.github.io/accessible-starter-astro/',
+  integrations: [
+    mdx(),
+    icon(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    compress(),
+  ],
 })
